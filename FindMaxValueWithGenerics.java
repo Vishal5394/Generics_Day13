01e60629b023136ff87fa8b2cad86599487bef9c
vishal5394 <vishaljadyal@gmail.com>
2022-07-26 16:06:08 +0530

    Refactor: Find Max. Using More Than 3 Parameter

--- FindMaxValueWithGenerics.java
+++ FindMaxValueWithGenerics.java
@@ -2,20 +2,24 @@ package com.BridgeLabz.generics;
 
 	public class FindMaxValueWithGenerics {
 
-		public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+		public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3, T a4, T a5) {
 			T max = a1;
 			if (a2.compareTo(max) > 0)
 				max = a2;
 			if (a3.compareTo(max) > 0)
 				max = a3;
+			if (a4.compareTo(max) > 0)
+				max = a4;
+			if (a5.compareTo(max) > 0)
+				max = a5;
 			return max;
 		}
 
 		public static void main(String[] args) {
 			System.out.println("Welcome To Find Maximun Problem Using Generics \n");
 			
-			System.out.println("The maximum value between the three integer is : " + maxOfValues(25, 18, 85)+ "\n");
-			System.out.println("The maximum value between the three float is : " + maxOfValues(2.5f, 4.56f, 7.87f) + "\n");
-			System.out.println("The maximum value between the three string is : " + maxOfValues("abc", "pqr", "jkl"));
+			System.out.println("The maximum value between the three integer is : " + maxOfValues(25, 18, 85, 34, 69)+ "\n");
+			System.out.println("The maximum value between the three float is : " + maxOfValues(2.5f, 4.56f, 7.87f, 8.54f, 5.64f) + "\n");
+			System.out.println("The maximum value between the three string is : " + maxOfValues("abc", "pqr", "jkl", "stu", "xyz"));
 		}
 	}
