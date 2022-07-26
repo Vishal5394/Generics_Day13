bd254529697484bf8c49cd3aed00d89b663cf35a
vishal5394 <vishaljadyal@gmail.com>
2022-07-26 14:43:45 +0530

    Added: Welcome to generics

---
+++ FindMaxValueWithGenerics.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz.generics;
+
+	public class FindMaxValueWithGenerics {
+
+		public static void main(String[] args) {
+			System.out.println("Welcome To Find Maximun Problem Using Generics \n");
+
+		}
+	}
